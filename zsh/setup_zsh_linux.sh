15cb8a53b0136f8ea63e68291d4c61db4285687d
Andrii Tytarenko <andriitytarenko@Andriis-MacBook-Pro.local>
2019-01-29 19:19:33 +0200

    Theme added.

--- zsh/setup_zsh_linux.sh
+++ zsh/setup_zsh_linux.sh
@@ -7,6 +7,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Download theme powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 # Copy config file
 cp ../zshrc $HOME/.zshrc
 
